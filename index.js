0b56ab65c65325435fff828badc4b85a12e18c97
bananaTiko <111482265+bananaTiko@users.noreply.github.com>
2024-01-17 15:56:06 -0800

    SHIT IS ABOUT TO GO DOWN

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.safeServers.forEach(x => { delete x.endpoint; delete x.substitutions; delete
 app.config = require('./settings.js')
 
 let rlMessage = "Rate limited ¯\\_(ツ)_/¯<br><br>Please do not spam my servers with a crazy amount of requests. It slows things down on my end and stresses RobTop's servers just as much." +
-" If you really want to send a zillion requests for whatever reason, please download the GDBrowser repository locally - or even just send the request directly to the GD servers.<br><br>" +
-"This kind of spam usually leads to GDBrowser getting IP banned by RobTop, and every time that happens I have to start making the rate limit even stricter. Please don't be the reason for that.<br><br>"
+" If you really want to send a zillion requests for whatever reason, please download the GDBrowser + repository locally - or even just send the request directly to the GD servers.<br><br>" +
+"This kind of spam usually leads to GDBrowser + getting IP banned by RobTop, and every time that happens I have to start making the rate limit even stricter. Please don't be the reason for that.<br><br>"
 
 const RL = rateLimit({
   windowMs: app.config.rateLimiting ? 5 * 60 * 1000 : 0,
